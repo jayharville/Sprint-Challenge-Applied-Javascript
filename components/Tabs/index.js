f74021cb159b2def365df88dde28ed00ca64d781
MacBook <Mac@MacBooks-MacBook-Pro.local>
2020-06-14 18:06:24 -0700

    completed task 1 created a Header component, completed task 2 Created tabs.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,41 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics").then(res=>{
+    const topics = res.data.topics;
+    const topicsContainer = document.querySelector(".topics");
+    topics.forEach(topic=>{
+        const tab = document.createElement("div");
+        tab.classList.add("tab");
+        tab.textContent = topic;
+        tab.dataset.category = topic;
+        tab.addEventListener("click", updateCategory);
+        topicsContainer.appendChild(tab);
+    })
+}).catch(err=>{
+    console.log(err);
+});
+
+function updateCategory(e){
+    let category = e.target.dataset.category;
+    //fixes inconsistency in naming of topics and article category
+    if (category === "node.js") category = "node";
+
+    axios.get("https://lambda-times-backend.herokuapp.com/articles").then(res=>{
+        const cardsContainer = document.querySelector(".cards-container");
+        cardsContainer.textContent = "";
+        const articles = res.data.articles;
+        for(key in articles){
+            if(key !== category) continue;
+            else {
+                articles[key].forEach(article=>{
+                    const card = createCard(article);
+                    cardsContainer.appendChild(card);
+                });
+            }
+        }
+    }).catch(err=>{
+        console.log(err);
+    });
+}
